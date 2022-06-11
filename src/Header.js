fa8ed86b0dfb7ad582e567a89eee53e7a76f906a
Dane <danefalberg@gmail.com>
2022-06-11 19:53:53 -0400

    gh pages workaround

--- src/Header.js
+++ src/Header.js
@@ -3,29 +3,17 @@ import React from "react";
 function Header() {
     return (
     <header className="jumbotron bg-light">
-        <div className="container-fluid text-black">
+        <div className="container-fluid">
           <h1 className="display-4 text-center">Price: Low to High</h1>
-          <nav class="navbar navbar-expand-lg bg-light align-center">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Click on of these to Go to a certain part of my website:</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/paintings">Paintings</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Something else eventually</a>
-                    </li>
-                </ul>
-                </div>
+          <div className="container">
+            <div>
+              <nav className=" navbar navbar-expand-lg bg-light">
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="/paintings">Paintings</a>
+                <a class="nav-link" href="/">Something else eventually, for right now also Home</a>
+              </nav>
             </div>
-          </nav>
+          </div>
         </div>
       </header>
     )
